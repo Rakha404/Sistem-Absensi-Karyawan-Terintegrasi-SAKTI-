5a4fb20e87a6816d33d21933612108b20c5d6c88
Rakha404 <dawgsyogs@gmail.com>
2026-04-16 12:20:34 +0700

    first commit

---
+++ src/main/java/com/mycompany/sistemsakti/SistemSAKTI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemsakti;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SistemSAKTI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
